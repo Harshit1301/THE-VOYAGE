3528bafff6da622d4107d6418d50c97bb355abab
Harshit1301 <singhharshit1301@gmail.com>
2025-02-28 18:22:36 +0530

    Update thevoyage.sql

--- Database/thevoyage.sql
+++ Database/thevoyage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2023 at 10:13 PM
+-- Generation Time: Dec 19, 2024 at 10:13 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
